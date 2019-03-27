f5f5f627ab94e70062d01d307d72f6752734223d
eliataylor <eli@taylormadetraffic.com>
2019-03-27 00:04:40 -0700

    snap & upload

--- imagerie-tools/imagerie-tools.js
+++ imagerie-tools/imagerie-tools.js
@@ -27,6 +27,10 @@ module.exports = function(RED) {
     //node.status({fill:"red",shape:"dot",text:"node-red:common.status.not-connected"});
   }
 
+  const postImage = function(node) {
+
+  }
+
   function PredictUpload(n) {
       RED.nodes.createNode(this,n);
       var config = RED.nodes.getNode(n.imagerieconfig);
@@ -95,6 +99,56 @@ module.exports = function(RED) {
       var node = this;
       node.project = n.project;
       node.camera = n.camera;
+
+      node.on('input', function(msg) {
+        if (!node.project) {
+          return alert("Select a project");
+        }
+        if (!node.camera) {
+          return alert("Select a camera");
+        }
+        var url = node.host + '/predict/snap/' + node.project + '/' + node.camera;
+        console.log("GET Image to " + url);
+
+        http.get(url, (res) => {
+          const { statusCode } = res;
+          const contentType = res.headers['content-type'];
+
+          let error;
+          if (statusCode !== 200) {
+            error = new Error('Request Failed.\n' +
+                              `Status Code: ${statusCode}`);
+          } else if (!/^application\/json/.test(contentType)) {
+            error = new Error('Invalid content-type.\n' +
+                              `Expected application/json but received ${contentType}`);
+          }
+          if (error) {
+            console.error(error.message);
+            // Consume response data to free up memory
+            res.resume();
+            return;
+          }
+
+          res.setEncoding('utf8');
+          let rawData = '';
+          res.on('data', (chunk) => { rawData += chunk; });
+          res.on('end', () => {
+            try {
+              const parsedData = JSON.parse(rawData);
+              console.log(parsedData);
+              node.send({
+                topic:'camera-snap-predicted',
+                payload:parsedData
+              });
+            } catch (e) {
+              console.error(e.message);
+            }
+          });
+        }).on('error', (e) => {
+          console.error(`Got error: ${e.message}`);
+        });
+
+      });
   }
 
   const httpMiddleware = function(req,res,next) {
